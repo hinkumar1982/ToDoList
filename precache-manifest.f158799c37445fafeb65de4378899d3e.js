cbfb48d83bd2c676cdb0c4bb6a668f9204666930
hinkumar1982 <hinkumar1982@gmail.com>
2020-05-25 18:41:04 +0530

    Updates

---
+++ precache-manifest.f158799c37445fafeb65de4378899d3e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e2b39f6e367ff864a8a77ad93ec3689d",
+    "url": "/ToDoList/index.html"
+  },
+  {
+    "revision": "ca1162ecfd5fc70044eb",
+    "url": "/ToDoList/static/css/main.400cc36f.chunk.css"
+  },
+  {
+    "revision": "3201ae916c4b98d1607b",
+    "url": "/ToDoList/static/js/2.3b1f09b3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ToDoList/static/js/2.3b1f09b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ca1162ecfd5fc70044eb",
+    "url": "/ToDoList/static/js/main.8c348557.chunk.js"
+  },
+  {
+    "revision": "59b870f6fc92aef76d46",
+    "url": "/ToDoList/static/js/runtime-main.a4a9a566.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ToDoList/static/media/logo.5d5d9eef.svg"
+  }
+]);
